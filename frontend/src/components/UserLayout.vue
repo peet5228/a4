890439fe13a5821406a87bf69bf88e8f07373546
peet5228 <ppeet5228@gmail.com>
2025-11-26 09:47:15 +0700

    edit vue

---
+++ frontend/src/components/UserLayout.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-app>
+        fff
+    </v-app>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import RegisterForm from '@/RegisterForm.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: '/regis',
+      name: 'RegisterForm',
+      component: RegisterForm,
     },
   ],
 })

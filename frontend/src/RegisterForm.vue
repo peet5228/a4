89c85de7b387e2d2b7aed2b42ef67304648fc4d7
peet5228 <ppeet5228@gmail.com>
2025-11-26 10:03:44 +0700

    edit vue

--- frontend/src/RegisterForm.vue
+++ frontend/src/RegisterForm.vue
@@ -1,6 +1,15 @@
 <template>
-    <v-container>
-        fff
+    <v-container fluid class="fill-height">
+        <v-row justify="center" align="center">
+            <v-col cols="12" md="8" lg="6">
+                <v-form>
+                    <v-card class="pa-4">
+                        <h1 class="text-center font-weight-bold text-h5">สมัครสมาชิก</h1>
+                        <p class="text-sm text-center mt-2">ระบบประเมินบุคลากร</p>
+                    </v-card>
+                </v-form>
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 

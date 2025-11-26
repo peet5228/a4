c0671b1010525d9bba36f0ac0be9b05702185441
peet5228 <ppeet5228@gmail.com>
2025-11-26 10:24:16 +0700

    update backend

--- frontend/src/RegisterForm.vue
+++ frontend/src/RegisterForm.vue
@@ -5,7 +5,12 @@
                 <v-form>
                     <v-card class="pa-4">
                         <h1 class="text-center font-weight-bold text-h5">สมัครสมาชิก</h1>
-                        <p class="text-sm text-center mt-2">ระบบประเมินบุคลากร</p>
+                        <p class="text-sm text-center mt-2">ระบบประเมินบุคลากรวิทยาลัยเทคนิคน่าน</p>
+                        <v-row class="mt-4">
+                            <v-col cols="12" md="6">
+                                <v-text-field v-model="form.first_name" :error-messages="form.first_name" label="ชื่อ"></v-text-field>
+                            </v-col>
+                        </v-row>
                     </v-card>
                 </v-form>
             </v-col>
@@ -14,6 +19,18 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const form = ref({
+    first_name:'',
+    last_name:'',
+    email:'',
+    username:'',
+    password:'',
+    role:'',
+})
+const confirmPassword = ref('')
+const error = ref<Record<string,string>>({})
 
 </script>
 
